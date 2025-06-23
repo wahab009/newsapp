807a899c1e3cd830f31e4dda18098822f2329242
Abdul Wahab <syedwahab99b@gmail.com>
2025-06-24 01:31:38 +0500

    Added the previous/next Page funtionalities

--- src/components/News.js
+++ src/components/News.js
@@ -1,22 +1,65 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem';
 
+
+
 export class News extends Component {
   constructor() {
     super();
     this.state = {
       articles: [],
-      loading: true
+      loading: true,
+      page:1,
+      totalPages:0
+
     };
   }
+  
+ 
+     
+ 
+  
 
+ 
   async componentDidMount() {
     console.log("cdm running");
-    let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=df14f9c4c05048af91aa2c76bab188a6";
+    let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=df14f9c4c05048af91aa2c76bab188a6&pageSize=6";
     let data = await fetch(url);
     let pressedData = await data.json();
     console.log("here is the data from API----->", pressedData);
     this.setState({ articles: pressedData.articles, loading: false });
+    let totalPages = Math.ceil(pressedData.totalResults / 6);
+    this.setState
+    ({
+      totalPages:totalPages
+    });
+
+  }
+   onNext = async ()=>{
+    console.log("Next was clicked");
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=df14f9c4c05048af91aa2c76bab188a6&pageSize=6&page=${this.state.page + 1}`;
+    let data = await fetch(url);
+    let pressedData = await data.json();
+    console.log("here is the data from API----->", pressedData);
+    this.setState({ 
+      
+      articles: pressedData.articles,
+      loading: false,
+      page: this.state.page+1 });
+  
+  }
+  onPrevious = async ()=>{
+console.log("previous was clicked");
+ let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=df14f9c4c05048af91aa2c76bab188a6&pageSize=6&page=${this.state.page - 1}`;
+    let data = await fetch(url);
+    let pressedData = await data.json();
+    console.log("here is the data from API----->", pressedData);
+    this.setState({ 
+     
+      articles: pressedData.articles,
+      loading: false,
+      page: this.state.page-1, });
+       
   }
 
   render() {
@@ -33,11 +76,15 @@ export class News extends Component {
                   title={element.title ? element.title: "Title not available"}
                   ImgUrl={element.urlToImage}
                   description={element.description ? element.description : "Click below for details"}
-                   newsurl={element.url}
+                   newsurl={element.url}                  
                 />
               </div>
             );
           })}
+        </div>
+        <div className="d-flex justify-content-between mt-3">
+          <button disabled={this.state.page <= 1 } type="button" onClick={this.onPrevious} class="btn btn-dark">&larr; Previous</button>
+          <button disabled ={this.state.page >= this.state.totalPages} type="button" onClick={this.onNext} class="btn btn-dark">Next &rarr;</button>
         </div>
       </div>
     );
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,7 +8,7 @@ export class NewsItem extends Component {
         <div className="card-body">
           <h5 className="card-title">{title}...</h5>
           <p className="card-text">{description}...</p>
-          <a href={newsurl} target ="_blank"  rel="noopener noreferrer" className="btn btn-primary">Read More</a>
+          <a href={newsurl} target ="_blank"  rel="noopener noreferrer" className="btn btn-dark">Read More</a>
         </div>
       </div>
     )
